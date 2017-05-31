efc623dc89c09e63fdf2b73bf034c267669264ec
Spyros Louvis <spyroslouvis@gmail.com>
2017-05-31 13:23:32 +0300

    Add files via upload

---
+++ BioLab/src/src/controllers/LoginController.java
@@ -0,0 +1,92 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.DoctorDAO;
+import dao.UserDAO;
+import model.Doctor;
+import model.User;
+//import model.User;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		RequestDispatcher loginfail = request.getRequestDispatcher("/index.html");//SOS allagi sto directory
+		RequestDispatcher loginsuccess = request.getRequestDispatcher("/index.html");//SOS allagi sto directory
+		
+		UserDAO udao = new UserDAO();
+		DoctorDAO ddao = new DoctorDAO();
+		
+		User user=null;
+		
+		
+		HttpSession session = request.getSession(true);
+		
+		try{
+			user = udao.confirmUser(username, password);
+			
+			if(user!=null){
+				int x = ddao.ifDoctor(username);
+				if(x==0){
+				session.setAttribute("user", user);
+				}else{
+					Doctor doctor = new Doctor(user.getName(),user.getSurname(),user.getEmail(),user.getUsername(),user.getPassword(),x);
+					session.setAttribute("user", doctor);
+				}
+				loginsuccess.forward(request, response);
+			}
+			else{
+				request.setAttribute("message", "Incorrect login");
+				loginfail.forward(request, response);
+			}
+		}catch (Exception e) {
+			
+			request.setAttribute("message", "You are not authorized to access this resource. Please login");
+			loginfail.forward(request, response);
+			
+		}
+		
+	}
+
+}
---
+++ BioLab/src/src/dao/MotherDAO.java
@@ -0,0 +1,87 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import connection.DB;
+import model.Mother;
+
+public class MotherDAO extends PatientDAO{
+	
+	public MotherDAO(){
+		super();
+	}
+	
+	public void saveMother(Mother mother) throws Exception{
+		savePatient(mother);
+		
+		DB db = new DB();
+		Connection con = null;
+		PreparedStatement st = null;
+		
+		String sqlq = "INSERT INTO bioproject.mother(afm) VALUES (?);";
+		
+		try{
+			db.open();
+			con=db.getConnection();
+			st=con.prepareStatement(sqlq);
+			st.setInt(1, mother.getAfm());
+			st.executeUpdate();
+			st.close();
+			db.close();
+		}catch(Exception e){
+			throw new Exception("An error occured while getting mothers from database: " + e.getMessage());
+		}finally{
+			try {
+				db.close();
+			} catch (Exception e) {
+				
+			} 
+		}
+	}
+	
+	public ArrayList<Mother> getMother() throws Exception{
+		
+		DB db= new DB();
+		Connection con = null;
+		String sqlquery = "SELECT * FROM bioproject.patient JOIN bioproject.mother ON bioproject.patient.patient_id=bioproject.mother.mother_id;";
+		ResultSet rs =null;
+		Statement st = null;
+		ArrayList<Mother> results = new ArrayList<Mother>();
+		
+		try{
+			db.open();
+			con = db.getConnection();
+			st = con.createStatement();
+			rs = st.executeQuery(sqlquery);
+			
+			while(rs.next()){
+				results.add(new Mother(rs.getString("patientId"),rs.getString("name"),rs.getString("surname"),rs.getString("HLA_A"),rs.getString("HLA_B"),rs.getString("HLA_C"),rs.getString("HLA_DPB1"),rs.getString("HLA_DQA1"),rs.getString("HLA_DQB1"),rs.getString("HLA_DRB1"),rs.getInt("afm")));
+			}
+			
+			
+			rs.close();
+			st.close();
+		
+			
+			return results;
+			
+		}catch(Exception e){
+			throw new Exception("An error occured while getting doctors from database: " + e.getMessage());
+		}finally {
+			
+			try {
+				db.close();
+			} catch (Exception e) {
+				
+			} 
+			
+		}
+		
+		
+	}
+
+}
